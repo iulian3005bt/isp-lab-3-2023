dd20ed31cfaf36554f01fdd63fc805089ced31a6
iulian3005bt <kon.iuli30@yahoo.com>
2023-03-20 21:53:07 +0200

    lab3

---
+++ src/main/java/isp/lab3/exercise1/Tree.java
@@ -0,0 +1,28 @@
+package isp.lab3.exercise1;
+
+public class Tree {
+    //atribute
+    int height;
+
+    //constructori
+    public Tree(){
+        this.height = 15;
+    }
+
+    public Tree (int height){
+        this.height = height;
+    }
+
+    //metode
+    public int grow(int meters){
+        if(meters>=1)
+        {this.height=this.height+meters;
+        }
+        return this.height;
+    }
+
+    public String toString(){
+        return this.height+"";
+    }
+
+}
